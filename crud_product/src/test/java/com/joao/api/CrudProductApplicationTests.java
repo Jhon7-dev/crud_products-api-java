bac16f4f24c206e23b1fc70cb4321412b0acaff8
João Victor Gomes de Souza <gomesjoao2003@gmail.com>
2025-12-09 12:49:49 -0300

    criação do projeto  crud_product

---
+++ crud_product/src/test/java/com/joao/api/CrudProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
